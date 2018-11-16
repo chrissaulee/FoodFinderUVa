58bd6922872a981007345b00385256a233785982
Chris <csl7dk@virginia.edu>
2018-11-16 15:45:52 -0500

    GPS and Device Shake

--- foodFinderApp/foodFinderApp/HomeViewController.swift
+++ foodFinderApp/foodFinderApp/HomeViewController.swift
@@ -7,16 +7,79 @@
 //
 
 import UIKit
+import CoreLocation
+import CoreMotion
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var GPSLabel: UILabel!
+    var locationManager:  CLLocationManager?
+    lazy var motionManager = CMMotionManager()
+    
+    // GPS
+    func createLocationManager(StartImmediately: Bool){
+        locationManager = CLLocationManager()
+        locationManager!.delegate = self
+        locationManager!.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager!.requestAlwaysAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled(){
+            locationManager!.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manageR: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+        let userLocation:CLLocation = locations[0] as CLLocation
+        let lat = String(userLocation.coordinate.latitude)
+        let long = String(userLocation.coordinate.longitude)
+        GPSLabel.text = lat + ", " + long
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController!.navigationBar.isHidden = true;
+        self.becomeFirstResponder()
+        
+        if CLLocationManager.locationServicesEnabled(){
+            switch CLLocationManager.authorizationStatus(){
+            case .authorizedAlways:
+                createLocationManager(StartImmediately: true)
+            case .authorizedWhenInUse:
+                createLocationManager(StartImmediately: true)
+            case .denied:
+                let alert = UIAlertController(title: "Denied", message: "Location Services are not enabled", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+                
+                self.present(alert, animated: true)
+            case .notDetermined:
+                createLocationManager(StartImmediately: false)
+                if let manager = self.locationManager{
+                    manager.requestWhenInUseAuthorization()
+                }
+            case .restricted:
+                let alert = UIAlertController(title: "Denied", message: "Location Services are not enabled", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            }
+        }
 
         // Do any additional setup after loading the view.
     }
     
+    // Device Shake
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            self.viewDidLoad()
+            print("reload")
+        }
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        get {
+            return true
+        }
+    }
 
     /*
     // MARK: - Navigation
